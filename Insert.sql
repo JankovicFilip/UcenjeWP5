4e4571c3d7bd0645b263ebf7b782ccbbc7c9f946
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-06-04 20:32:54 +0200

    Ucenje inserta

---
+++ Insert.sql
@@ -0,0 +1,23 @@
+﻿select * from smjerovi;
+
+-- skolska sintaksa
+-- 1 -> ovo je sifra koju generira baza
+insert into smjerovi (naziv,cijena,brojsati,izvodiseod,vaucer)
+values ('Web programiranje',1599.99,225,'2024-02-07 18:00', 1);
+
+
+-- losa sintaksa
+-- 2
+insert into smjerovi values ('Web dizajn',1200,150,null,null);
+
+-- mvp
+-- 3
+insert into smjerovi (naziv) values ('ččččšđ');
+
+select * from grupe;
+--1
+insert into grupe (naziv, brojslobodnihmjesta, smjer)
+values ('WP5',26,1);
+--2
+insert into grupe (naziv, brojslobodnihmjesta, smjer)
+values ('WD1',16,2);
