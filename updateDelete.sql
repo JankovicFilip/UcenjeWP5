3a47daede6fb118c58e7f47f9576bd07e0273a77
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-06-06 20:41:28 +0200

    update polaznika, set oib

--- updateDelete.sql
+++ updateDelete.sql
@@ -1,7 +1,21 @@
-select * from smjerovi where sifra=2;
+--select * from smjerovi where sifra=2;
 
-update smjerovi set izvodiseod='2024-06-06 19:10' where sifra=2;
+--update smjerovi set izvodiseod='2024-06-06 19:10' where sifra=2;
 
-update smjerovi set cijena = 999.42 , vaucer=0 where sifra=3;
+--update smjerovi set cijena = 999.42 , vaucer=0 where sifra=3;
 
+--select * from smjerovi;
+--update smjerovi set cijena = cijena * 0.9;
+--select * from smjerovi;
 
+--select * from smjerovi;
+--update smjerovi set cijena = cijena * 1.065;
+--select * from smjerovi;
+
+--select * from smjerovi;
+--update smjerovi set cijena = cijena - 10;
+--select * from smjerovi;
+
+select * from polaznici;
+update polaznici set oib = '07759048912' where sifra = 5;
+select * from polaznici;
