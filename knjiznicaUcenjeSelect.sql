011675c1836dd0fe06c6c94826303c8f4a47a22b
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-06-11 20:51:04 +0200

    ucenje selecta iz knjiznice

--- knjiznicaUcenjeSelect.sql
+++ knjiznicaUcenjeSelect.sql
@@ -1,7 +1,25 @@
--- select top 10 percent * from autor;
+﻿-- select top 10 percent * from autor;
 
-select distinct ime from autor
-where ime not like '%a%'
-and ime like 't%'
-and ime not like '%o%'
-order by ime;
+--select distinct ime from autor
+--where ime not like '%k%'
+--and ime like 'z%'
+--and ime not like '%a%'
+--order by ime;
+
+--select * from izdavac;
+
+-- select * from izdavac where aktivan is null;
+
+--update izdavac
+--set aktivan = 0
+--where aktivan is null;
+
+
+
+--insert into autor(sifra,ime,prezime,datumrodenja) values
+--(4, 'Filip', 'Janković', '1993-11-11 11:00');
+-- select * from autor;
+
+
+
+-- select * from mjesto where postanskiBroj like '31%';
