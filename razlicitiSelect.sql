757057051ffb5bfd12de3be6c7e839fc3c5bc6ec
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-06-11 19:57:44 +0200

    logicki i ostali select operatori

--- razlicitiSelect.sql
+++ razlicitiSelect.sql
@@ -1,4 +1,4 @@
-select * from smjerovi;
+-- select * from smjerovi;
 
 -- filtriranje kolona
 -- * sve kolone
@@ -11,3 +11,39 @@ select * from smjerovi;
 
 
 -- filtriranja redova
+
+select * from polaznici;
+
+-- where klauzula
+-- operatori = , != razlicito
+
+select * from polaznici where ime = 'Luka';
+
+select * from polaznici where ime != 'Luka';
+
+-- < > >= <=
+select * from polaznici where sifra < 5;
+
+-- logicki operatori
+-- and, or, not
+-- boolean operatori
+select ime, prezime from polaznici where sifra > 5 and sifra < 10;
+
+select * from polaznici where sifra = 1 or sifra = 2;
+
+select * from polaznici where not sifra = 2
+
+-- ostali operatori
+-- like
+
+select * from polaznici where ime not like '%a%';
+
+select * from polaznici where ime like '%a%';
+
+-- descending
+select ime from polaznici where ime like '%a%'
+order by ime desc;
+
+-- distinct radi da ako ima 2 filipa, pokaze jednog
+select distinct ime from polaznici where ime like '%a%'
+order by ime desc;
