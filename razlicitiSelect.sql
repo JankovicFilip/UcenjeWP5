7c4c2859e430a2efe3b3fd2b9877be8cfc870114
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-06-11 19:41:46 +0200

    razliciti nacin da se odradi select

---
+++ razlicitiSelect.sql
@@ -0,0 +1,13 @@
+select * from smjerovi;
+
+-- filtriranje kolona
+-- * sve kolone
+-- naziv kolone, konstanta i izraz
+
+--select naziv, cijena, *,
+--'Osijek' as grad,2 as broj,
+--getdate() as datum from smjerovi;
+
+
+
+-- filtriranja redova
