641fb4691444791cfa9f25b95b5a78c5a4c5a2f7
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-06-18 20:36:45 +0200

    select preko razlicitih tablica koje nemaju dignuti vanjski kljuc

--- svastaraSelect.sql
+++ svastaraSelect.sql
@@ -19,3 +19,22 @@ on c.sifra = b.zupanija
 where c.naziv like '%split%'
 group by b.naziv
 order by 2 desc;
+
+
+select top 5 a.naziv,
+sum(c.kolicina*c.cijena) as ukupno,
+min(c.kolicina*c.cijena) as minimalno,
+max(c.kolicina*c.cijena) as maksimalno
+from Dobavljaci a inner join Primke b
+on a.sifra = b.dobavljac
+inner join ArtikliNaPrimci c
+on c.primka = b.sifra
+where b.datum between '2017-01-01' and '2017-12-31'
+group by a.naziv
+having min(c.kolicina*c.cijena) > 0
+order by 3 desc, 2 desc;
+
+select a.*
+from Kupci a inner join Mjesta b
+on a.mjesto = b.sifra
+where b.naziv in('Osijek','Punitovci');
