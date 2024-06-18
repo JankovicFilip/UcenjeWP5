1e241d197cecdc7985db21c54c4c3c18e2b62e3b
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-06-18 19:21:25 +0200

    svastara select

--- svastaraSelect.sql
+++ svastaraSelect.sql
@@ -1,5 +1,5 @@
 ﻿--use svastara;
 
-select * from Artikli where cijena >= 100 and cijena <=150 and dugiNaziv like 'kućanski aparat';
+select * from Artikli where  kratkiNaziv like '%PLAFONJERA%' and cijena >= 100 and cijena <=150;
 
 
