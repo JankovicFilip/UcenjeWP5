191a340c571774c69f077da331521780650f8255
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-07-02 21:20:02 +0200

    pocetak ciklicne tablice

---
+++ CSHARP/Ucenje/UcenjeCS/CiklicnaTablica.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class CiklicnaTablica
+    {
+
+        public static void Izvedi()
+        {
+            Console.Write("Broj redova: ");
+            int red = int.Parse(Console.ReadLine());
+            Console.Write("Broj stupaca: ");
+            int stupac = int.Parse(Console.ReadLine());
+
+            int[,] ciklicnaTablica = new int [red, stupac];
+
+
+
+
+
+        }
+    }
+}
