7afd09ff34cf2cd12725e9eefcd43c10dfd6081b
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-07-09 21:59:16 +0200

    Ljubavni kalkulator, ime moze sadrzavati samo slova i ne moze biti prazno

--- CSHARP/Ucenje/UcenjeCS/E12LjubavniKalkulator.cs
+++ CSHARP/Ucenje/UcenjeCS/E12LjubavniKalkulator.cs
@@ -10,8 +10,9 @@ namespace UcenjeCS
     {
         public static void Izvedi()
         {
-            string prvoIme = UnesiIme("Unesi prvo ime: ");
-            string drugoIme = UnesiIme("Unesi drugo ime: ");
+            string prvoIme = UnesiIme("Unesi prvo ime: ").ToUpper();
+            string drugoIme = UnesiIme("Unesi drugo ime: ").ToUpper();
+            Console.WriteLine("Unijeli ste {0} i {1}.", prvoIme, drugoIme);
         }
 
         static string UnesiIme(string poruka)
@@ -27,23 +28,34 @@ namespace UcenjeCS
                     Console.WriteLine("Niste unijeli ime!");
                     continue;
                 }
+
+                bool isValid = true;
+                foreach (char c in ime)
+                {
+                    if (!char.IsLetter(c))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Ime može sadržavati samo slova!");
+                    continue;
+                }
+
                 return ime;
 
             } while (true);
-
-            Console.WriteLine("Unijeli ste: >{0}<", ime);
-
-
-
-
         }
 
 
 
-            
 
 
-        
+
+
 
 
     }
