89fe73fe626e3ed56b511f375bc9a2a712901169
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-09-03 18:02:14 +0200

    osoba pero

--- CSHARP/Ucenje/UcenjeCS/E15KlasaObjekt/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/E15KlasaObjekt/Program.cs
@@ -16,6 +16,21 @@ namespace UcenjeCS.E15KlasaObjekt
             Console.WriteLine("Hello oop!");
 
             // objekt je pojavnost klase iliti instanca klase
+
+            // Osoba je ime klase
+            // osoba je ime objekta (instanca, pojavnost) - varijabla
+
+            //Osoba osoba = new Osoba();
+
+            Osoba osoba; // deklariran bez instance
+            osoba = new Osoba(); // konstruiranje objekta - rađa se / kreira se, zauzima memoriju
+            osoba.Ime = "Pero"; // objekt zivi i posjeduje vrijednost - postavljamo ih
+
+            Console.WriteLine(osoba.Ime); // tu se vrijednosti konzumiraju
+
+
+            // na kraju objekt (osoba) umire - unistava ga/cisti iz memorije
+            // mi ne unistavamo objekt, to radi garbage collector
         }
     }
 }
