8c4eb7518d2b1efb7675a7be2c2a79bfd2640ae4
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-09-12 17:04:15 +0200

    Subota tecaj

--- CSHARP/Ucenje/UcenjeCS/E18KonzolnaAplikacija/Izbornik.cs
+++ CSHARP/Ucenje/UcenjeCS/E18KonzolnaAplikacija/Izbornik.cs
@@ -1,61 +1,105 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UcenjeCS.E18KonzolnaAplikacija.Model;
 
 namespace UcenjeCS.E18KonzolnaAplikacija
 {
-    public class Izbornik
+    internal class Izbornik
     {
+
         public ObradaSmjer ObradaSmjer { get; set; }
+        public ObradaPolaznik ObradaPolaznik { get; set; }
         public ObradaGrupa ObradaGrupa { get; set; }
+
         public Izbornik()
         {
             Pomocno.DEV = true;
             ObradaSmjer = new ObradaSmjer();
+            ObradaPolaznik = new ObradaPolaznik();
             ObradaGrupa = new ObradaGrupa(this);
+            UcitajPodatke();
+            PozdravnaPoruka();
             PrikaziIzbornik();
+        }
 
+        private void UcitajPodatke()
+        {
+            string docPath =
+         Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
+            if (File.Exists(Path.Combine(docPath, "smjerovi.json")))
+            {
+                StreamReader file = File.OpenText(Path.Combine(docPath, "smjerovi.json"));
+                ObradaSmjer.Smjerovi = JsonConvert.DeserializeObject<List<Smjer>>(file.ReadToEnd());
+                file.Close();
 
+            }
 
         }
 
         private void PrikaziIzbornik()
         {
             Console.WriteLine("Glavni izbornik");
-            Console.WriteLine("1 Smjerovi");
-            Console.WriteLine("2 Grupe");
-            Console.WriteLine("3 Polaznici");
-            Console.WriteLine("4 Izlaz iz programa");
+            Console.WriteLine("1. Smjerovi");
+            Console.WriteLine("2. Polaznici");
+            Console.WriteLine("3. Grupe");
+            Console.WriteLine("4. Izlaz iz programa");
             OdabirOpcijeIzbornika();
         }
 
         private void OdabirOpcijeIzbornika()
         {
-            switch(E11Metode.UcitajCijeliBroj("Odaberi stavku izbornika: ", 1, 4))
+
+            switch (Pomocno.UcitajRasponBroja("Odaberite stavku izbornika", 1, 4))
             {
                 case 1:
-                    ObradaSmjer.PrikaziIzbornik();
                     Console.Clear();
+                    ObradaSmjer.PrikaziIzbornik();
                     PrikaziIzbornik();
                     break;
                 case 2:
-                    ObradaGrupa.PrikaziIzbornik();
                     Console.Clear();
+                    ObradaPolaznik.PrikaziIzbornik();
+                    PrikaziIzbornik();
+                    break;
+                case 3:
+                    Console.Clear();
+                    ObradaGrupa.PrikaziIzbornik();
                     PrikaziIzbornik();
                     break;
                 case 4:
-                    Console.WriteLine("Dovidenja");
+                    Console.WriteLine("Hvala na korištenju aplikacije, doviđenja!");
+                    SpremiPodatke();
                     break;
-
-
-
             }
-            
-            
+        }
 
+        private void SpremiPodatke()
+        {
+            if (Pomocno.DEV)
+            {
+                return;
+            }
+
+            //Console.WriteLine(JsonConvert.SerializeObject(ObradaSmjer.Smjerovi));
+
+            string docPath =
+          Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "smjerovi.json"));
+            outputFile.WriteLine(JsonConvert.SerializeObject(ObradaSmjer.Smjerovi));
+            outputFile.Close();
+        }
+
+        private void PozdravnaPoruka()
+        {
+            Console.WriteLine("*********************************");
+            Console.WriteLine("*** Edunova Console App v 1.0 ***");
+            Console.WriteLine("*********************************");
         }
     }
 }
--- CSHARP/Ucenje/UcenjeCS/E18KonzolnaAplikacija/ObradaGrupa.cs
+++ CSHARP/Ucenje/UcenjeCS/E18KonzolnaAplikacija/ObradaGrupa.cs
@@ -7,96 +7,159 @@ using UcenjeCS.E18KonzolnaAplikacija.Model;
 
 namespace UcenjeCS.E18KonzolnaAplikacija
 {
-    public class ObradaGrupa
+    internal class ObradaGrupa
     {
-        public List<Grupa> Grupe { get; set; }
-        public Izbornik Izbornik { get; set; }
-        public ObradaGrupa(Izbornik Izbornik)
-        {
-            this.Izbornik = Izbornik;
-            Grupe = new List<Grupa>();
-            if(Pomocno.DEV)
-            {
-                UcitajTestnePodatke();
-            }
 
+        public List<Grupa> Grupe { get; set; }
+        private Izbornik Izbornik;
+
+        public ObradaGrupa()
+        {
+            Grupe = new List<Grupa>();
 
         }
-
-        
-
-        private void UcitajTestnePodatke()
+        public ObradaGrupa(Izbornik izbornik) : this()
         {
-            Grupe.Add(new() { Sifra = 1, Naziv = "WP5", Smjer = Izbornik.ObradaSmjer.Smjerovi[0] });
-            Grupe.Add(new() { Sifra = 1, Naziv = "WP4", Smjer = Izbornik.ObradaSmjer.Smjerovi[1] });
+            this.Izbornik = izbornik;
         }
 
         public void PrikaziIzbornik()
         {
-            Console.WriteLine("Rad s grupama");
-            Console.WriteLine("1 Prikazi sve grupe");
-            Console.WriteLine("2 Dodaj novi grupu");
-            Console.WriteLine("3 Promijeni");
-            Console.WriteLine("4 Obrisi");
-            Console.WriteLine("5 Vrati na glavni izbornik");
+            Console.WriteLine("Izbornik za rad s grupama");
+            Console.WriteLine("1. Pregled svih grupa");
+            Console.WriteLine("2. Unos nove grupe");
+            Console.WriteLine("3. Promjena podataka postojeće grupe");
+            Console.WriteLine("4. Brisanje grupe");
+            Console.WriteLine("5. Povratak na glavni izbornik");
             OdabirOpcijeIzbornika();
-
-
-
-
         }
 
         private void OdabirOpcijeIzbornika()
         {
-            switch(E11Metode.UcitajCijeliBroj("Odaberi stavku izbornika", 1, 5))
+            switch (Pomocno.UcitajRasponBroja("Odaberite stavku izbornika", 1, 5))
             {
                 case 1:
-                    PrikaziSveGrupe();
+                    PrikaziGrupe();
                     PrikaziIzbornik();
                     break;
                 case 2:
-                    DodajNovuGrupu();
+                    UnosNoveGrupe();
+                    PrikaziIzbornik();
+                    break;
+                case 3:
+                    PromjeniPodatkeGrupe();
+                    PrikaziIzbornik();
+                    break;
+                case 4:
+                    ObrisiGrupu();
                     PrikaziIzbornik();
                     break;
                 case 5:
+                    Console.Clear();
                     break;
-                
             }
         }
 
-        private void DodajNovuGrupu()
+        private void ObrisiGrupu()
         {
-            var g = new Grupa();
-            g.Sifra = E11Metode.UcitajCijeliBroj("Unesi sifru grupe",1,int.MaxValue);
-            g.Naziv = Pomocno.UcitajString("Unesi naziv grupe");
-            var ios = Izbornik.ObradaSmjer;
-            ios.PrikaziSveSmjerove();
-            g.Smjer = ios.Smjerovi[
-                E11Metode.UcitajCijeliBroj("Odaberite smjer za ovu grupu",1,ios.Smjerovi.Count) -1
+            PrikaziGrupe();
+            var g = Grupe[
+                Pomocno.UcitajRasponBroja("Odaberi redni broj grupe za brisanje", 1, Grupe.Count) - 1
                 ];
-            Grupe.Add( g );
+            if (Pomocno.UcitajBool("Sigurno obrisati " + g.Naziv + "? (DA/NE)", "da"))
+            {
+                Grupe.Remove(g);
+            }
+        }
+
+        private void PromjeniPodatkeGrupe()
+        {
+            PrikaziGrupe();
+            var g = Grupe[
+                Pomocno.UcitajRasponBroja("Odaberi redni broj grupe za promjenu", 1, Grupe.Count) - 1
+                ];
+            // copy paste s linije 102 - izvući u metodu
+            g.Sifra = Pomocno.UcitajRasponBroja("Unesi šifru grupe", 1, int.MaxValue);
+            g.Naziv = Pomocno.UcitajString("Unesi naziv grupe", 50, true);
+            //smjer
+            Izbornik.ObradaSmjer.PrikaziSmjerove();
+
+            g.Smjer = Izbornik.ObradaSmjer.Smjerovi[
+                Pomocno.UcitajRasponBroja("Odaberi redni broj smjera", 1, Izbornik.ObradaSmjer.Smjerovi.Count) - 1];
+
+            g.Predavac = Pomocno.UcitajString("Unesi ime i prezime predavača", 50, true);
+            g.MaksimalnoPolaznika = Pomocno.UcitajRasponBroja("Unesi maksimalno polaznika", 1, 30);
+
+            // polaznici
+            g.Polaznici = UcitajPolaznike((int)g.MaksimalnoPolaznika);
+
 
         }
 
-        private void PrikaziSveGrupe()
+        private void PrikaziGrupe()
         {
-            if (Grupe.Count == 0) // ovo je povezano sa linijom 14 iz Izbornik klase, obavijesti te da su smjerovi prazni, u ovom slucaju ako je DEV false
+            Console.WriteLine("*****************************");
+            Console.WriteLine("Grupe u aplikaciji");
+            int rb = 0, rbp;
+            foreach (var g in Grupe)
             {
-                var staraB = Console.BackgroundColor;
-                var staraF = Console.ForegroundColor;
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Nema smjerova u bazi");
-                Console.BackgroundColor = staraB;
-                Console.ForegroundColor = staraF;
+                Console.WriteLine(++rb + ". " + g.Naziv + " (" + g.Smjer?.Naziv + "), " + g.Polaznici?.Count + " polaznika"); // prepisati metodu toString
+                rbp = 0;
+                g.Polaznici.Sort();
+                foreach (var p in g.Polaznici)
+                {
+                    Console.WriteLine("\t" + ++rbp + ". " + p.Ime + " " + p.Prezime);
+                }
+            }
+            Console.WriteLine("****************************");
+        }
 
-                return;
-            }
-            var rb = 0;
-            foreach (var e in Grupe)
+        private void UnosNoveGrupe()
+        {
+            Console.WriteLine("***************************");
+            Console.WriteLine("Unesite tražene podatke o grupi");
+
+            Grupa g = new Grupa();
+            g.Sifra = Pomocno.UcitajRasponBroja("Unesi šifru grupe", 1, int.MaxValue);
+            g.Naziv = Pomocno.UcitajString("Unesi naziv grupe", 50, true);
+            //smjer
+            Izbornik.ObradaSmjer.PrikaziSmjerove();
+
+            g.Smjer = Izbornik.ObradaSmjer.Smjerovi[
+                Pomocno.UcitajRasponBroja("Odaberi redni broj smjera", 1, Izbornik.ObradaSmjer.Smjerovi.Count) - 1];
+
+            g.Predavac = Pomocno.UcitajString("Unesi ime i prezime predavača", 50, true);
+            g.MaksimalnoPolaznika = Pomocno.UcitajRasponBroja("Unesi maksimalno polaznika", 1, 30);
+
+            // polaznici
+            g.Polaznici = UcitajPolaznike((int)g.MaksimalnoPolaznika);
+
+            Grupe.Add(g);
+        }
+
+        private List<Polaznik> UcitajPolaznike(int maksimalnoPolaznika)
+        {
+            List<Polaznik> lista = new List<Polaznik>();
+            while (lista.Count < maksimalnoPolaznika && Pomocno.UcitajBool("Za unos polaznika unesi DA", "da"))
             {
-                Console.WriteLine(++rb + ". " + e.Naziv + "(" + e.Smjer?.Naziv + ")" );
+                Izbornik.ObradaPolaznik.PrikaziPolaznike();
+                Console.WriteLine((Izbornik.ObradaPolaznik.Polaznici.Count + 1) + ". Dodaj novog polaznika");
+                var odabranaOpcija = Pomocno.UcitajRasponBroja("Odaberi redni broj polaznika ili zadnji broj za dodavanje novog", 1,
+                        Izbornik.ObradaPolaznik.Polaznici.Count + 1);
+                if (odabranaOpcija == Izbornik.ObradaPolaznik.Polaznici.Count + 1)
+                {
+                    // ide novi
+                    Izbornik.ObradaPolaznik.UnosNovogPolaznika();
+                    lista.Add(Izbornik.ObradaPolaznik.Polaznici.LastOrDefault());
+                }
+                else
+                {
+                    lista.Add(Izbornik.ObradaPolaznik.Polaznici[odabranaOpcija]);
+                }
+
             }
+
+            return lista;
         }
     }
 }
